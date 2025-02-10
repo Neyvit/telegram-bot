640c812dd0869002ac320fc389d06cca41f95b7b
egor <zrobko.egor@wb.ru>
2025-02-10 20:59:11 +0300

    restructured project

---
+++ common/constants.py
@@ -0,0 +1,13 @@
+class CallbackData:
+    CARDS = "cards"
+    INFOGRAPHICS = "infographics"
+    BUSINESS_CARDS = "business_cards"
+    LOGOS = "logos"
+    PRICE_LISTS = "price_lists"
+    LEAFLETS = "leaflets"
+    CERTIFICATES = "certificates"
+    PORTFOLIO = "portfolio"
+
+class URLs:
+    PORTFOLIO = "https://www.behance.net/gallery/213576091/Portfolio"
+    DESIGNER = "https://t.me/Angelika_zhu" 
